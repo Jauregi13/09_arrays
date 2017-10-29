9a08e5eab623343cae81ff6ccf7b547c8b68f810
Jon Jauregi <Jon Jauregi@DESKTOP-UPFRNRT>
2017-10-29 22:17:03 +0100

    Ejercicios resueltos

--- src/Asteriscos.java
+++ src/Asteriscos.java
@@ -2,10 +2,16 @@
 public class Asteriscos {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 5; i++){
-			for (int j = 0; j < 3; j++){
-				System.out.print("*");
+		String[][] asterisco = new String[5][5];
+		String caracter = "*";
+		for (int i = 0; i < asterisco.length; i++){
+			int indice = 0;
+			for (int j = 0; j < asterisco[i].length; j++){
+				if(j == indice)
+				asterisco[i][j]= caracter;
+				System.out.print(asterisco[i][j]);
 			}
+			System.out.println("");
 		}
 
 	}
--- src/OrdenarArray.java
+++ src/OrdenarArray.java
@@ -5,23 +5,38 @@ public class OrdenarArray {
 	public static void main(String[] args) {
 		Scanner lector = new Scanner(System.in);
 		int[] ordenado = new int[5];
-		int aux = 0;
 		// Añadir los valores al array
 		for (int i = 0; i < ordenado.length; i++){
 			System.out.println("Escribe numero "+(i+1)+":");
 			ordenado[i]= lector.nextInt();
 		}
-		for(int i = 0; i < ordenado.length - i; i++){
+		// mostrar valores del array
+		System.out.println("Estos son los valores que nos has dado:");
+		for(int i = 0; i< ordenado.length; i++){
+			System.out.print(ordenado[i]+" ");
+		}
+		System.out.println("");
+		System.out.println("Estos son los valores de menor a mayor:");
+		for(int i = 0; i < ordenado.length; i++){
 			for (int j = 0; j < ordenado.length; j++){
-				if (ordenado[j] < ordenado[j+1]){
+				int aux = 0;
+				if (j == ordenado.length -1){
+					
+				}
+				else if(ordenado[j] > ordenado[j+1]){
 					aux = ordenado[j];
 					ordenado[j] = ordenado[j+1];
 					ordenado[j+1] = aux;
+					
 				}
+				
 	
 			}
+			
 		}
-		
+		for(int i = 0; i< ordenado.length; i++){
+			System.out.print(ordenado[i]+" ");
+		}
 
 	}
 
