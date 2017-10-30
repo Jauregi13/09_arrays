726ab3ce042552a11515e9793ad635e5f31a6319
Jon Jauregi <Jon Jauregi@DESKTOP-UPFRNRT>
2017-10-31 00:57:50 +0100

    Ejercicio asteriscos terminado

--- src/Asteriscos.java
+++ src/Asteriscos.java
@@ -2,14 +2,19 @@
 public class Asteriscos {
 
 	public static void main(String[] args) {
-		String[][] asterisco = new String[5][5];
+		String[][] asterisco = new String[6][6];
 		String caracter = "*";
 		for (int i = 0; i < asterisco.length; i++){
 			int indice = 0;
 			for (int j = 0; j < asterisco[i].length; j++){
-				if(j == indice)
-				asterisco[i][j]= caracter;
-				System.out.print(asterisco[i][j]);
+				if(j < i+1){
+					asterisco[i][j]= caracter;
+					System.out.print(asterisco[i][j]);
+				}
+				else{
+					asterisco[i][j]= "";
+					System.out.print(asterisco[i][j]);
+				}
 			}
 			System.out.println("");
 		}
